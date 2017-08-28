fe0bd1f1bf3f1fdd0205aec76baa3258906ca477
Andrey Bondarenko <ent101@outlook.com>
2017-08-28 20:09:00 +0300

    Доработано: вывод подробной информации при нажатии на аватарки

--- js/map.js
+++ js/map.js
@@ -1,7 +1,16 @@
 'use strict';
 
 var baseValuesOffer = {
-  'titles': ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'],
+  'titles': [
+    'Большая уютная квартира',
+    'Маленькая неуютная квартира',
+    'Огромный прекрасный дворец',
+    'Маленький ужасный дворец',
+    'Красивый гостевой домик',
+    'Некрасивый негостеприимный домик',
+    'Уютное бунгало далеко от моря',
+    'Неуютное бунгало по колено в воде'
+  ],
   'type': ['flat', 'house', 'bungalo'],
   'checkin': ['12:00', '13:00', '14:00'],
   'checkout': ['12:00', '13:00', '14:00'],
@@ -23,35 +32,29 @@ var baseTypesOffer = {
   'bungalo': 'Бунгало'
 };
 
+var keysCodes = {
+  ESC: 27,
+  ENTER: 13
+};
+
 var countOffers = 8;
 var lodgeTemplate = document.querySelector('#lodge-template').content;
-
-/**
- * Создает копию массива
- *
- * @param {array} items массив значений
- * @return копию массива
- */
+var tokyoPinMap = document.querySelector('.tokyo__pin-map');
+var offerDialog = document.querySelector('#offer-dialog');
+var oldPin = null;
 
 /**
  * Случайное кол-во элементов из массива
  *
- * @param {array} items
+ * @param {array} array
  * @return массив элементов
  */
 
-var resortItems = function (items) {
-  items.counter = 0;
+var resortItems = function (array) {
   var tmpArray = [];
-  var length = items.length;
-  var currentLength = getRandomInt(1, length);
-
+  var currentLength = getRandomInt(1, array.length);
   for (var i = 0; i < currentLength; i++) {
-    var elementPosition = getRandomInt(items.counter, length - 1);
-    var element = items[elementPosition];
-    items[elementPosition] = items.splice(items.counter, 1, items[elementPosition])[0];
-    items.counter = items.counter === length - 1 ? 0 : items.counter + 1;
-    tmpArray.push(element);
+    tmpArray.push(getUniqueItem(array, i));
   }
   return tmpArray;
 };
@@ -73,20 +76,21 @@ var getRandomInt = function (min, max) {
 /**
  * Случайные элемент массива
  *
- * @param {array} items принимает массив
+ * @param {array} array принимает массив
  * @return случайный элемент массива
  */
 
-var getRandomItem = function (items) {
-  var length = items.length;
+var getRandomItem = function (array) {
+  var length = array.length;
   var randomItem = getRandomInt(0, length - 1);
-  return items[randomItem];
+  return array[randomItem];
 };
 
 /**
  * Возвращает уникальный элемент из массива
  *
- * @param {array} items принимает массив
+ * @param {array} array принимает массив
+ * @param {int} i число
  * @return уникальный элемент из массива
  */
 
@@ -144,29 +148,24 @@ var createOffers = function (numOffers) {
 /**
  * создание блока автарки с указанием стилей и позиции размещения на карте
  *
- * @param {obj} items массов объектов предложений
+ * @param {obj} array массив объектов предложений
  * @param {int} imgWidth ширина аватарки
  * @param {int} imgHeight высота аватарки
  * @return HTML блок для аватарки
  */
 
-var createAvatarBlock = function (items, imgWidth, imgHeight, counter) {
+var createAvatarBlock = function (array, imgWidth, imgHeight, counter) {
   var pinBlock = document.createElement('div');
   var imgBlock = document.createElement('img');
-  imgBlock.dataset.countNumber = counter;
-  imgBlock.setAttribute('tabindex', '0');
-
+  pinBlock.dataset.countNumber = counter;
+  pinBlock.tabIndex = 0;
   pinBlock.className = 'pin';
-  pinBlock.style.left = (items['location']['x'] - imgWidth / 2) + 'px';
-  /*
-  при вычислении по Y аватарки попадают в зону над горизонтом,
-  baseValuesOffer['minY'] = 100 маловато значение, если поставить 200 будет выглядеть лучше
-  */
-  pinBlock.style.top = (items['location']['y'] - imgHeight) + 'px';
+  pinBlock.style.left = (array['location']['x'] - imgWidth / 2) + 'px';
+  pinBlock.style.top = (array['location']['y'] - imgHeight) + 'px';
   imgBlock.className = 'rounded';
   imgBlock.width = 40;
   imgBlock.height = 40;
-  imgBlock.src = items['author']['avatar'];
+  imgBlock.src = array['author']['avatar'];
   pinBlock.appendChild(imgBlock);
   return pinBlock;
 };
@@ -174,16 +173,16 @@ var createAvatarBlock = function (items, imgWidth, imgHeight, counter) {
 /**
  * добавление аватарок в HTML
  *
- * @param {obj} items items массов объектов предложений
+ * @param {obj} array массив объектов предложений
  * @param {int} imgWidth ширина аватарки
  * @param {int} imgHeight высота аватарки
  */
 
-var createAvatars = function (items, imgWidth, imgHeight) {
+var createAvatars = function (array, imgWidth, imgHeight) {
   var avatarBlock = document.querySelector('.tokyo__pin-map');
   var fragment = document.createDocumentFragment();
-  for (var i = 0; i < items.length; i++) {
-    fragment.appendChild(createAvatarBlock(items[i], imgWidth, imgHeight, i));
+  for (var i = 0; i < array.length; i++) {
+    fragment.appendChild(createAvatarBlock(array[i], imgWidth, imgHeight, i));
   }
   avatarBlock.appendChild(fragment);
 };
@@ -191,97 +190,150 @@ var createAvatars = function (items, imgWidth, imgHeight) {
 /**
  * Вывод информационного блока (слева вверху) с описанием предложения и информацией
  *
- * @param {array} items - первый объект из массива предложений
- * @param {any} template - шаблон который используется для создания контента
+ * @param {array} array - первый объект из массива предложений
  */
 
-
-
-var createDialog = function (items) {
+var createDialog = function (array) {
   var lodgeItem = lodgeTemplate.cloneNode(true);
-  var lodgeTitle = lodgeItem.querySelector('.lodge__title');
-  var lodgeAddress = lodgeItem.querySelector('.lodge__address');
-  var lodgePrice = lodgeItem.querySelector('.lodge__price');
-  var lodgeType = lodgeItem.querySelector('.lodge__type');
-  var lodgeRooms = lodgeItem.querySelector('.lodge__rooms-and-guests');
-  var lodgeCheckin = lodgeItem.querySelector('.lodge__checkin-time');
   var dialog = document.querySelector('.dialog');
   var dialogPanel = dialog.querySelector('.dialog__panel');
 
-  lodgeTitle.textContent = items['offer']['title'];
-  lodgeAddress.textContent = items['offer']['address'];
-  lodgePrice.innerHTML = items['offer']['price'] + '&#x20bd;/ночь';
-  lodgeType.textContent = baseTypesOffer[items['offer']['type']];
-  lodgeRooms.textContent = 'Для ' + items['offer']['guests'] + ' гостей в ' + items['offer']['rooms'] + ' комнатах';
-  lodgeCheckin.textContent = 'Заезд после ' + items['offer']['checkin'] + ', выезд до ' + items['offer']['checkout'];
+  lodgeItem.querySelector('.lodge__title').textContent = array['offer']['title'];
+  lodgeItem.querySelector('.lodge__address').textContent = array['offer']['address'];
+  lodgeItem.querySelector('.lodge__price').innerHTML = array['offer']['price'] + '&#x20bd;/ночь';
+  lodgeItem.querySelector('.lodge__type').textContent = baseTypesOffer[array['offer']['type']];
+  lodgeItem.querySelector('.lodge__rooms-and-guests').textContent = 'Для ' + array['offer']['guests'] + ' гостей в ' + array['offer']['rooms'] + ' комнатах';
+  lodgeItem.querySelector('.lodge__checkin-time').textContent = 'Заезд после ' + array['offer']['checkin'] + ', выезд до ' + array['offer']['checkout'];
 
-  for (var i = 0; i < items['offer']['features'].length; i++) {
+  for (var i = 0; i < array['offer']['features'].length; i++) {
     var span = document.createElement('span');
-    span.className = 'feature__image feature__image--' + items['offer']['features'][i];
+    span.className = 'feature__image feature__image--' + array['offer']['features'][i];
     lodgeItem.querySelector('.lodge__features').appendChild(span);
   }
 
-  lodgeItem.querySelector('.lodge__description').textContent = items['offer']['description'];
-  dialog.querySelector('.dialog__title img').src = items['author']['avatar'];
+  lodgeItem.querySelector('.lodge__description').textContent = array['offer']['description'];
+  dialog.querySelector('.dialog__title img').src = array['author']['avatar'];
 
   dialog.replaceChild(lodgeItem, dialogPanel);
 };
 
 var currentOffers = createOffers(countOffers);
 createAvatars(currentOffers, baseValuesOffer['pinWidth'], baseValuesOffer['pinHeight']);
-createDialog(currentOffers[0]);
 
-//+++++++++++++++++++++++++++++++++++++++++++
-//+++++++++++++++++++++++++++++++++++++++++++
+/**
+ * Производит поиск по родителя содержащий искомый css класс
+ * Поднимается вверж от child пока не встретит родтеля с классом selector
+ *
+ * @param {obj} объект
+ * @param {string} css class родителя
+ * @returns возвращает найденный родитель с указанным css классом
+ */
 
-var keysCodes = {
-  ESC: 27,
-  ENTER: 13
-};
-var tokyoPinMap = document.querySelector('.tokyo__pin-map');
-var pinsTokyoPinMap = tokyoPinMap.querySelectorAll('.pin:not(.pin__main)');
-var offerDialog = document.querySelector('#offer-dialog');
-var offerDialogClose = offerDialog.querySelector('.dialog__close');
-
-var pinClickHendler = function (event) {
-  if ((event.keyCode === keysCodes['ENTER'] || event.type === 'click') && (event.target.tagName.toLowerCase() === 'img')) {
-    var target = event.currentTarget;
-    removeCurrentActivePin();
-    addClassToCurrentPin(target);
-    dialogOpenWindow(event, currentOffers);
+function getParentBySelector(child, selector) {
+  var node = child;
+  while (node && !node.classList.contains(selector)) {
+    node = node.parentElement;
   }
-};
-
-var addClassToCurrentPin = function (target) {
-  target.classList.add('pin--active');
-};
-
-var removeCurrentActivePin = function () {
-  var currentActivePin = tokyoPinMap.querySelector('.pin--active');
-  if (currentActivePin) {
-    currentActivePin.classList.remove('pin--active');
-  }
-};
-
-var dialogOpenWindow = function (event, array) {
-  var i = event.target.dataset.countNumber;
-  createDialog(array[i]);
-  offerDialog.classList.remove('hidden');
-};
-
-var dialogCloseClickHendler = function () {
-  if (event.keyCode === keysCodes['ESC'] || event.type === 'click') {
-    offerDialog.classList.add('hidden');
-    removeCurrentActivePin();
-  }
-};
-
-
-for (var i = 0; i < pinsTokyoPinMap.length; i++) {
-  pinsTokyoPinMap[i].addEventListener('click', pinClickHendler);
-  pinsTokyoPinMap[i].addEventListener('keydown', pinClickHendler);
+  return node;
 }
 
+/**
+ * закрывает окно с информацией о предложении (слево вверху)
+ *
+ */
 
-offerDialogClose.addEventListener('click', dialogCloseClickHendler);
-window.addEventListener('keydown', dialogCloseClickHendler);
+function closeDialog() {
+  if (!offerDialog.classList.contains('hidden')) {
+    offerDialog.classList.add('hidden');
+  }
+}
+
+/**
+ * показывает окно с информацией о предложении,
+ * добавлет класс pin--active к выбранной автарке
+ * проверяет если на данный момент у другого pin (отличного от выбранного) класс pin--active и удаляет его
+ *
+ */
+
+function showAdDetails() {
+  var pin = getParentBySelector(event.target, 'pin');
+  if (pin && !pin.classList.contains('pin__main')) {
+    pin.classList.add('pin--active');
+    if (oldPin && oldPin !== pin) {
+      oldPin.classList.remove('pin--active');
+    }
+    oldPin = pin;
+    createDialog(currentOffers[pin.dataset.countNumber]);
+    if (offerDialog.classList.contains('hidden')) {
+      offerDialog.classList.remove('hidden');
+    }
+  }
+}
+
+/**
+ * скрывает окно с информацией о предолжении и убирает подсветку у активной автарки на карте
+ *
+ */
+
+function doHiddenAdDetails() {
+  offerDialog.classList.add('hidden');
+  if (oldPin) {
+    oldPin.classList.remove('pin--active');
+  }
+}
+
+/**
+ * закртие окна инфрмации о предложении
+ *
+ * @param {any} event
+ */
+
+function hiddenAdDetails(event) {
+  if (getParentBySelector(event.target, 'dialog__close')) {
+    if (!offerDialog.classList.contains('hidden')) {
+      doHiddenAdDetails();
+    }
+  }
+}
+
+function clickHandler(calldack) {
+  return function (event) {
+    calldack(event);
+  };
+}
+
+function entterPressHandler(callback) {
+  return function (event) {
+    if (event.keyCode === keysCodes['ENTER']) {
+      callback(event);
+    }
+  };
+}
+/*
+скрывает окно с информацией
+*/
+closeDialog();
+
+/*
+вешаем обработчики на аватарки расположенные на карте. клик мышки на автарке, enter на автарке в фокусе
+*/
+tokyoPinMap.addEventListener('click', clickHandler(showAdDetails));
+tokyoPinMap.addEventListener('keydown', entterPressHandler(showAdDetails));
+
+/*
+вешаем обработчики на окно с подробной информацией о предолжении. клик мышки на крестике и enter на кнопке закрыто окно
+*/
+offerDialog.addEventListener('click', clickHandler(hiddenAdDetails));
+offerDialog.addEventListener('keydown', entterPressHandler(hiddenAdDetails));
+
+/*
+ вешаем обработчики на окно с подробной информацией о предолжении. закрытие по нажатию esc
+ */
+document.addEventListener('keydown', function (event) {
+  if (event.keyCode === keysCodes['ESC']) {
+    if (!offerDialog.classList.contains('hidden')) {
+      doHiddenAdDetails();
+    }
+  }
+});
+
