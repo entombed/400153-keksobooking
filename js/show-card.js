cc7857f7ad0ea7abfd2045f97acebe7988da8215
Andrey Bondarenko <ent101@outlook.com>
2017-09-03 21:03:01 +0300

    Исправлено: удаление класса pin--active при закрытии dialog

--- js/show-card.js
+++ js/show-card.js
@@ -1,7 +1,5 @@
 'use strict';
 (function () {
-  /* переменная в которую записывается текущий элемент с классом pin--active */
-  var oldPin = null;
 
   /* переменная для работы с окном (dialog) подробной информации о предложении */
   var offerDialog = document.querySelector('#offer-dialog');
@@ -17,10 +15,10 @@
     var pin = window.getParentBySelector(event.target, 'pin');
     if (pin && !pin.classList.contains('pin__main')) {
       pin.classList.add('pin--active');
-      if (oldPin && oldPin !== pin) {
-        oldPin.classList.remove('pin--active');
+      if (window.oldPin && window.oldPin !== pin) {
+        window.oldPin.classList.remove('pin--active');
       }
-      oldPin = pin;
+      window.oldPin = pin;
       window.createDialog(window.currentOffers[pin.dataset.countNumber]);
       if (offerDialog.classList.contains('hidden')) {
         offerDialog.classList.remove('hidden');
@@ -30,6 +28,5 @@
 
   /* экпортируем в глобальную зону видимости */
   window.showCard = showDialogDetails;
-  window.oldPin = oldPin;
 
 })();
