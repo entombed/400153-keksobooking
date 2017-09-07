cbee841d9765b5861e78e8e78c8ab704076321c2
Andrew Bondarenko <ent101@outlook.com>
2017-09-07 15:55:28 +0300

    В работе: фильтр для предложений

--- js/filter.js
+++ js/filter.js
@@ -3,28 +3,48 @@
 (function () {
   var avatarBlock = document.querySelector('.tokyo__pin-map'); // template для создания pin
 
+
   var filterPins = function (event, array, filter) {
     switch (event.target.name.toLowerCase()) {
       case 'housing_type':
-        console.log(filter['housing_type']['value']);
+        //console.log(filter['housing_type']['value']);
+        var check = 'type'
+        sortArray(array, filter['housing_type']['value'], check);
         break;
       case 'housing_price':
-        console.log(filter['housing_price']['value']);
+//        console.log(filter['housing_price']['value']);
         break;
       case 'housing_room-number':
-        console.log(filter['housing_room-number']['value']);
+//        console.log(filter['housing_room-number']['value']);
+        var check = 'rooms';
+        sortArray(array, filter['housing_room-number']['value'], check);
         break;
       case 'housing_guests-number':
-        console.log(filter['housing_guests-number']['value']);
+//        console.log(filter['housing_guests-number']['value']);
+        var check = 'guests';
+        sortArray(array, filter['housing_guests-number']['value'], check);
         break;
     }
   };
 
+  var sortArray = function (array, data, check) {
+
+    if (data == 'any') {
+      newA = array;
+    } else {
+      var newA = array.filter(function (room) {
+        return room.offer[check] == data;
+      });
+    }
+    clearMap();
+    window.pin.createPins(newA, avatarBlock);
+  }
+
   var clearMap = function () {
-      var pins = avatarBlock.querySelectorAll('.pin:not(.pin__main)');
-      for (var i = 0; i < pins.length; i++) {
-        pins[i].parentNode.removeChild(pins[i]);
-      }
+    var pins =  avatarBlock.querySelectorAll('.pin:not(.pin__main)');
+    for (var i = 0; i < pins.length; i++) {
+      pins[i].parentNode.removeChild(pins[i]);
+    }
   };
   window.filter = {
     filterPins: filterPins
