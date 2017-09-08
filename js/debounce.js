bbe25c2f63eedcf8d61c002a3bd8a66788fba867
Andrey Bondarenko <ent101@outlook.com>
2017-09-08 20:54:04 +0300

    Реализовано: функция debounce

--- js/debounce.js
+++ js/debounce.js
@@ -1,13 +1,18 @@
 'use strict';
 
 (function () {
-  var DEBOUNCE_INTERVAL = 300;
+  var DEBOUNCE_INTERVAL = 2000;
   var lastTimeout = null;
-  var debounce = function (fun) {
+  var debounce = function () {
+    var args = Array.prototype.slice.call(arguments);
+    var fun = args[0];
     if (lastTimeout) {
       window.clearTimeout(lastTimeout);
     }
-    lastTimeout = window.setTimeout(fun, DEBOUNCE_INTERVAL);
+    lastTimeout = window.setTimeout(function () {
+      args.splice(0, 1);
+      fun.apply(null, args);
+    }, DEBOUNCE_INTERVAL);
   };
 
   /* экспортируем в глобальную область видимости */
